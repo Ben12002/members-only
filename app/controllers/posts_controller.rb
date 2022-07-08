968ff517bc29266217c9f92f5f66c04e1ad9cbab
Benedict Tan <bendamian2012@gmail.com>
2022-07-08 09:24:36 -0700

    Completed functionality, partially styled index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -9,5 +10,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    # Do this instead of Post.new(post_params), so that user_id gets filled automatically.
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, uniqueness: true, presence: true
+  validates :body, presence: true
+  belongs_to :user
 end
